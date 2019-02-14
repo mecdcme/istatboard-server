6ec1ab4e59fc436baaa95688a14545d9ebef1a88
Francesco Amato <framato@pc.istat.it>
2019-02-14 16:16:57 +0100

    a

--- app/models/timereport.model.js
+++ app/models/timereport.model.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
 	const timereport = sequelize.define('timereport', {
 	  user:Sequelize.STRING,
-		moods: Sequelize.STRING,
-		timeps: Sequelize.STRING
+		moods: Sequelize.STRING
+	 
 		
 	}, {
 		tableName: 'view_user_mood_time'
