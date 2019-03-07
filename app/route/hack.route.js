e52b7ba623173aeaa3b086445b7329178d4be3df
Francesco Amato <framato@pc.istat.it>
2019-03-07 15:00:48 +0100

    file upload

--- app/route/hack.route.js
+++ app/route/hack.route.js
@@ -8,6 +8,10 @@ module.exports = function (app) {
   const timereport = db.timereport;
   const euMainActivityRate = db.EuMainActivityRate;
 
+  
+const multer = require('multer');
+const path = require('path');
+
   // Retrieve all ilogdiaries
   app.get('/api/ilogdiary',
     function (req, res) {
@@ -157,4 +161,41 @@ function (req, res) {
       getReportData(res, rows[0].vtable);
     });
 });
-  }
+
+
+
+
+
+
+
+
+const DIR = './uploads';
+ 
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() +  path.extname(file.originalname));
+    }
+});
+let upload = multer({storage: storage});
+ 
+
+
+app.post('/api/upload',upload.single('photo'), function (req, res) {
+  if (!req.file) {
+      console.log("No file received");
+      return res.send({
+        success: false
+      });
+  
+    } else {
+      console.log('file received');
+      return res.send({
+        success: true
+      })
+    }
+});
+
+  }//end file
--- server.js
+++ server.js
@@ -3,13 +3,25 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
+
+//upload photo
+app.use(bodyParser.urlencoded({extended: true}));
+ 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+  
 const cors = require('cors')
 const corsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200
 }
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 const db = require('./app/config/db.config.js');
   
@@ -30,3 +42,5 @@ var server = app.listen(8080, function () {
 
   console.log("App listening at http://%s:%s", host, port);
 })
+
+
